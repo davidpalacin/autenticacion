19b3ca083062a17f005ac0b701bd964ba49f8e16
davidpalacin <palacingimenezdavid.fpllefia@gmail.com>
2024-11-04 13:21:25 +0100

    fix

--- src/controllers/userController/deleteUser.ts
+++ src/controllers/userController/deleteUser.ts
@@ -4,7 +4,7 @@ import { DeleteUserPayloadInterface } from "../../utils/interfaces/DeleteUserPay
 async function deleteUser(payload: DeleteUserPayloadInterface) {
   const id = payload.id
   if (!id) throw new Error('Ha ocurrido un error.')
-  const userDelete = await deleteUserDb(id)
+  await deleteUserDb(id)
 
   return {
     message: "User deleted",
